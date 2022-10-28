8a126bf9b47890e04f58c7ea43597df5531f9229
Musa Doci <81824102+doci-git@users.noreply.github.com>
2022-10-28 18:50:20 +0200

    js classes

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const btn = document.querySelector('.btn');
 const content = document.querySelector('.content');
 const img = document.querySelector('img');
 
-btn.addEventListener('click',()=>{
-fetch(url).then((data)=> data.json()).then((risposta)=>getdata(risposta))
+btn.addEventListener('click', () => {
+    fetch(url).then((data) => data.json()).then((risposta) => getdata(risposta))
 })
 
 function getdata(data) {
-    const { 
-        value:jokes
-    }= data;
-    content.textContent= jokes;
-    img.classList.add('')
+    const {
+        value: jokes
+    } = data;
+    content.textContent = jokes;
+    img.classList.add('img-shake')
+    setTimeout(() => {
+        img.classList.remove('img-shake')
+    }, 400);
 }
